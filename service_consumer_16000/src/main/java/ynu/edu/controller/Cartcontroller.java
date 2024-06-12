531b573f352e6a0b45e7330c77036974807aa92c
地球人拒绝进化 <1376361629@qq.com>
2024-06-12 23:58:31 +0800

    Your commit message

--- service_consumer_16000/src/main/java/ynu/edu/controller/Cartcontroller.java
+++ service_consumer_16000/src/main/java/ynu/edu/controller/Cartcontroller.java
@@ -3,6 +3,7 @@ package ynu.edu.controller;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -70,4 +71,7 @@ public class Cartcontroller {
         CommonResult<User> result = new CommonResult<User>(400,"fallback",new User());
         return result;
     }
+
+
+
 }
